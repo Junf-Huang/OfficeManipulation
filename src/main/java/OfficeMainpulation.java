3c701494da88136b9077e4d50a66509cfff35150
Junf-Huang <1205331774@qq.com>
2018-07-05 23:56:55 +0800

    you can insert one row or rows data into excel.

--- src/main/java/OfficeMainpulation.java
+++ src/main/java/OfficeMainpulation.java
@@ -1,29 +1,22 @@
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-
-import java.io.FileOutputStream;
+import office.Excel;
 
 public class OfficeMainpulation {
 
     public static void main(String []args) throws Exception{
 
-        Workbook wb=new HSSFWorkbook();
-//创建sheet1
-        Sheet sheet = wb.createSheet("sheet1");
-//创建sheet2
-        wb.createSheet("hjj");
-//创建行
-        Row row = sheet.createRow(0);
-//创建单元格
-        row.createCell(0).setCellValue("省份证号");
-        row.createCell(1).setCellValue("年龄");
-        row.createCell(2).setCellValue("性别");
-        row.createCell(3).setCellValue("身高");
-        //创建一个输入流
-        FileOutputStream fileOutputStream = new FileOutputStream("/home/junf/Documents/test.xlsx");
-//写入
-        wb.write(fileOutputStream);
+        String excelName = "test";
+        String sheetName = "sheet1";
+        String title = "list";
+        String[] value2={"张三","李四", "小伙伴","最近","比较忙", "大家"};
+        String[] value1={"张三","李四", "小伙伴","最近","比较忙"};
+
+        Excel excel = new Excel(excelName);
+        excel.switchSheet(sheetName);
+        excel.createTitle(title);
+        excel.createRows(value1, 2);
+        excel.createOneRow(value2);
+        excel.save();
     }
+
+
 }
--- src/main/java/office/Excel.java
+++ src/main/java/office/Excel.java
@@ -1,47 +1,130 @@
 package office;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.util.List;
 
 public class Excel {
 
-    public static final String XLSX_FILE_PATH = "/home/junf/Documents/";
-    private String xlsxName;
+    private static final String XLSX_FILE_PATH = "/home/junf/Documents/";
+    private String xlsx;
     private Workbook wb;
+    private Sheet currentSheet;
 
+
+    //initalize Excel
     public Excel(String xlsxName) {
 
-        this.xlsxName = XLSX_FILE_PATH + xlsxName + ".xlsx";
-        // Creating a Workbook from an Excel file (.xls or .xlsx)
+        this.xlsx = XLSX_FILE_PATH + xlsxName + ".xlsx";
+        //Creating a Workbook from an Excel file (.xls or .xlsx)
         this.wb = new HSSFWorkbook();
-        //创建sheet1
-        Sheet sheet = wb.createSheet("sheet1");
-
-        //创建行
-        Row row = sheet.createRow(0);
-        //创建单元格
-        row.createCell(0).setCellValue("省份证号");
-        row.createCell(1).setCellValue("年龄");
-        row.createCell(2).setCellValue("性别");
-        row.createCell(3).setCellValue("身高");
     }
 
-    public void createSheet(String sheetName) {
-        wb.createSheet(sheetName);
+    //if exit sheet, switch it, else create new sheet
+    public Sheet switchSheet(String sheetName) {
+        if (getSheet(sheetName)==null)
+            this.currentSheet = wb.createSheet(sheetName);
+        return this.currentSheet;
     }
 
+    //get special sheet
     public Sheet getSheet(String sheetName) {
         return wb.getSheet(sheetName);
     }
 
-    public void saveXlsx() throws Exception {
+    //insert a special column and set cell value.
+    public void createOneRow(int NoRow, String[] values) {
 
+        Row row = currentSheet.createRow(NoRow);
+        for(int i = 0; i < values.length; i++ ) {
+            row.createCell(i).setCellValue(values[i]);
+        }
+
+    }
+
+    //insert into a row and set values
+    public void createOneRow(String[] values) {
+        Row row = currentSheet.createRow(currentSheet.getLastRowNum()+1);
+        for(int i = 0; i < values.length; i++ ) {
+            row.createCell(i).setCellValue(values[i]);
+        }
+    }
+
+    //insert into rows and set values
+    public void createRows(String[] values, int columnNum) {
+
+        int lastRow = currentSheet.getLastRowNum()+1;
+
+        int rowNum;
+        if(values.length%columnNum==0)
+            rowNum = values.length/columnNum;
+        else
+            rowNum = values.length/columnNum+1;
+
+        for (int i = 0; i < rowNum; i++) {
+            Row row = currentSheet.createRow(i+lastRow);
+            for(int j = 0; j < columnNum; j++) {
+                if(j+i*columnNum >= values.length)
+                    break;
+                row.createCell(j).setCellValue(values[j+i*columnNum]);
+            }
+        }
+    }
+
+    public void createTitle(String title){
+        if(this.currentSheet!=null)
+            currentSheet.createRow(0).createCell(0).setCellValue(title);
+        else
+            System.out.println("Please choose a sheet");
+    }
+
+    //save excel to the special path
+    public void save() throws Exception {
         //创建一个输入流
-        FileOutputStream fileOutputStream = new FileOutputStream("/home/junf/Documents/test.xlsx");
+        FileOutputStream fileOutputStream = new FileOutputStream(xlsx);
         //写入
         wb.write(fileOutputStream);
     }
+
+    private static void printCellValue(Cell cell) {
+        switch (cell.getCellTypeEnum()) {
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+            case STRING:
+                System.out.print(cell.getRichStringCellValue().getString());
+                break;
+            case NUMERIC:
+                if (DateUtil.isCellDateFormatted(cell)) {
+                    System.out.print(cell.getDateCellValue());
+                } else {
+                    System.out.print(cell.getNumericCellValue());
+                }
+                break;
+            case FORMULA:
+                System.out.print(cell.getCellFormula());
+                break;
+            case BLANK:
+                System.out.print("");
+                break;
+            default:
+                System.out.print("");
+        }
+
+        System.out.print("\t");
+    }
+
+
+    //read excel
+
+    public void read() throws Exception{
+        HSSFWorkbook workbook = new HSSFWorkbook(new FileInputStream(xlsx));
+        HSSFSheet sheet = null;
+    }
+
 }
